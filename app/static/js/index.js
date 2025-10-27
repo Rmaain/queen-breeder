d3c986f468f2e3ac65691d062b5ef6880306715d
Rmaain <intornanow@gmail.com>
2025-10-26 21:40:47 -0300

    new files

---
+++ app/static/js/index.js
@@ -0,0 +1,9 @@
+document.body.addEventListener("click", (env) => {
+    const isExandableTitle = !!env.target.closest('.expandable--title')
+    const expandable = env.target.closest('.expandable')
+    if(!isExandableTitle){
+        return
+    }
+    expandable.classList.toggle('expandable--open')
+
+})
